7dbc3dbc9f9cabf225cb452dcc71bc37471077f4
Rayyan2001 <paudelanurag123@gmail.com>
2024-08-21 11:09:46 +0545

    changes

---
+++ mainapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+  
+    path('visualization/', views.visualize_csv_form),
+    path('predict', views.predict),
+    path('finding/',views.finding),
+    path('chatbot/question/', views.chatbot_question, name='chatbot_question'),
+    
+]
